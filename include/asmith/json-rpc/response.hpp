ef2d4ae6fa1315a0d1035e15a32bb2196b1a2459
Adam Smith <adam.george.smith@btinternet.com>
2017-05-02 22:45:54 +0100

    Built in error codes

--- include/asmith/json-rpc/response.hpp
+++ include/asmith/json-rpc/response.hpp
@@ -19,10 +19,20 @@
 #include "rpc.hpp"
 
 namespace asmith { namespace rpc {
+	enum {
+		PARSE_ERROR			= -32700,
+		INVALID_REQUEST		= -32600,
+		METHOD_NOT_FOUND	= -32601,
+		INVALID_PARAMS		= -32602,
+		INTERNAL_ERROR		= -32603,
+		SERVER_ERROR_MIN	= -32000,
+		SERVER_ERROR_MAX	= -32099
+	};
+
 	struct error {
 		serial::value data;
 		std::string message;
-		uint64_t code;
+		int64_t code;
 	};
 
 	struct response {
